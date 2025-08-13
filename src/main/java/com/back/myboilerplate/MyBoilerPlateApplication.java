0243de53c2bdf4019ad625331b1e0873c1065412
정동하 <jeongdongha@jeongdonghaui-MacBookAir.local>
2025-08-13 16:19:37 +0900

    work

---
+++ src/main/java/com/back/myboilerplate/MyBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.back.myboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBoilerPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBoilerPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/myboilerplate/MyBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.myboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBoilerPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
